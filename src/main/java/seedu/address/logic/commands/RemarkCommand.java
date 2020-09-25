2ada0adced5d0ce5e3d9f04d8ea9074e9eaaedd0
Low Ming Lim <48002577+Avalionnet@users.noreply.github.com>
2020-09-25 16:02:47 +0800

    Implement tests for RemarkCommand

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -17,9 +17,10 @@ import seedu.address.model.person.Remark;
  */
 public class RemarkCommand extends Command{
 
-    public static final String COMMAND_WORD = "remark";
     public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
     public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
+
+    public static final String COMMAND_WORD = "remark";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
             + "by the index number used in the last person listing. "
             + "Existing remark will be overwritten by the input.\n"
@@ -27,6 +28,7 @@ public class RemarkCommand extends Command{
             + "r/ [REMARK]\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + "r/ Likes to swim.";
+
 
     private final Index index;
     private final Remark remark;
