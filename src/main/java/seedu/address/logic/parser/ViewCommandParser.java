78e2af12147b54da08e196a5fc3e282dfb3ab6a7
Zhang_Wanlin <zhangwanlin98@gmail.com>
2020-10-13 09:39:39 +0800

    update javadoc

--- src/main/java/seedu/address/logic/parser/ViewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewCommandParser.java
@@ -17,6 +17,7 @@ public class ViewCommandParser implements Parser<ViewCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the ViewCommand
      * and returns a ViewCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format.
      */
     @Override
