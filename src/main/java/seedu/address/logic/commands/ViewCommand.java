3800d24a9f5750d637ea22ace16308a8ab430b51
JinHao-L <limjinhao@gmail.com>
2020-10-13 16:08:40 +0800

    Update display message of view all

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -3,10 +3,10 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_APPOINTMENTS_OVERVIEW;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPOINTMENTS;
 
 import java.util.function.Predicate;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.appointment.Appointment;
 
@@ -24,19 +24,13 @@ public class ViewCommand extends Command {
             + COMMAND_WORD + " "
             + PREFIX_DATE + "12-Dec-2021 ";
 
-    public static final String MESSAGE_SUCCESS = "Viewed all Appointments";
+    public static final String MESSAGE_SUCCESS = "Viewing all Appointments";
 
     private final Predicate<Appointment> predicate;
 
     /**
      * Creates a ViewCommand to list the appointments on a date.
-     */
-    public ViewCommand() {
-        this.predicate = null;
-    }
-
-    /**
-     * Creates a ViewCommand to list the appointments on a date.
+     *
      * @param predicate date of the appointments.
      */
     public ViewCommand(Predicate<Appointment> predicate) {
@@ -46,9 +40,14 @@ public class ViewCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
+    public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredAppointmentList(predicate);
+
+        if (predicate.equals(PREDICATE_SHOW_ALL_APPOINTMENTS)) {
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
+
         return new CommandResult(
                 String.format(
                         MESSAGE_APPOINTMENTS_OVERVIEW, model.getFilteredAppointmentList().size())
@@ -59,6 +58,6 @@ public class ViewCommand extends Command {
     public boolean equals(Object other) {
         return other == this
                 || (other instanceof ViewCommand
-                    && predicate.equals(((ViewCommand) other).predicate));
+                && predicate.equals(((ViewCommand) other).predicate));
     }
 }
