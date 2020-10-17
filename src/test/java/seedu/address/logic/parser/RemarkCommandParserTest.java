3f37717c6fbac0552d7ec357c8c5ca11fa4d700e
Low Ming Lim <48002577+Avalionnet@users.noreply.github.com>
2020-10-17 16:47:15 +0800

    Add RemarkCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -0,0 +1,71 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.ASSIGN_DATE;
+import static seedu.address.logic.commands.CommandTestUtil.ASSIGN_TIME;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_DATE_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_REMARK_EXCEED_LIMIT;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_REMARK_INDEX;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_TIME_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_TIME_DESC_CLOSED;
+import static seedu.address.logic.commands.CommandTestUtil.REMARK_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.REMARK_DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.REMARK_DESC_EMPTY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PATIENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PATIENT;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.model.appointment.Time;
+import seedu.address.model.patient.Remark;
+import seedu.address.testutil.DateTimeLoaderBuilder;
+
+public class RemarkCommandParserTest {
+
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE);
+
+    private final RemarkCommandParser parser = new RemarkCommandParser();
+
+    @Test
+    public void parse_missingParts_failure() {
+        // no remark specified
+        //assertParseFailure(parser, " 1 ", MESSAGE_INVALID_FORMAT);
+
+        // no index specified
+        assertParseFailure(parser, PREFIX_REMARK + VALID_REMARK_AMY, MESSAGE_INVALID_FORMAT);
+
+        // no index and no index and remark specified
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+
+        // invalid index
+        assertParseFailure(parser, INVALID_REMARK_INDEX,
+                MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidValue_failure() {
+        assertParseFailure(parser, INVALID_REMARK_EXCEED_LIMIT, Remark.MESSAGE_CONSTRAINTS);
+    }
+
+    @Test
+    public void parse_allFieldPresents_success() {
+        String userInput = REMARK_DESC_AMY;
+        RemarkCommand expectedCommand = new RemarkCommand(INDEX_FIRST_PATIENT, new Remark(VALID_REMARK_AMY));
+        assertParseSuccess(parser, userInput, expectedCommand);
+
+        String userInput2 = REMARK_DESC_BOB;
+        RemarkCommand expectedCommand2 = new RemarkCommand(INDEX_SECOND_PATIENT, new Remark(VALID_REMARK_BOB));
+        assertParseSuccess(parser, userInput2, expectedCommand2);
+
+        String userInput3 = REMARK_DESC_EMPTY;
+        RemarkCommand expectedCommand3 = new RemarkCommand(INDEX_FIRST_PATIENT, new Remark(""));
+        assertParseSuccess(parser, userInput3, expectedCommand3);
+    }
+}
