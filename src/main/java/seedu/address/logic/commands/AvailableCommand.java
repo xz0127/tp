bd1bf5ffd23a0ce876a39e7bf647e8be1a2d392c
xz0127 <xinzhe0127@gmail.com>
2020-10-24 19:42:04 +0800

    Add AvailableCommand and AvailableCommandParser classes

---
+++ src/main/java/seedu/address/logic/commands/AvailableCommand.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.appointment.Appointment;
+
+/**
+ * Lists all available time slots on a specific date.
+ */
+public class AvailableCommand extends Command {
+
+    public static final String COMMAND_WORD = "avail";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all available time slots [on a date]\n"
+        + "Parameters: "
+        + "[" + PREFIX_DATE + "DATE]\n"
+        + "Example: " + COMMAND_WORD + " "
+        + PREFIX_DATE + "12-Apr-2021 ";
+
+    public static final String MESSAGE_SUCCESS = "Listing all available time slots";
+
+    private final Predicate<Appointment> predicate;
+
+    /**
+     * Creates a AvailableCommand to find all available time slots on a date.
+     *
+     * @param predicate date of the appointments.
+     */
+    public AvailableCommand(Predicate<Appointment> predicate) {
+        requireNonNull(predicate);
+
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.updateFilteredAppointmentList(predicate);
+
+        List<Appointment> lastShownAppointmentList = model.getFilteredAppointmentList();
+        String timeSlotsMessage = model.findAvailableTimeSlots(lastShownAppointmentList);
+
+        return new CommandResult(MESSAGE_SUCCESS + timeSlotsMessage);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+            || (other instanceof AvailableCommand
+            && predicate.equals(((AvailableCommand) other).predicate));
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/AvailableCommandParser.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+
+import seedu.address.logic.commands.AvailableCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.Date;
+import seedu.address.model.appointment.DateMatchesPredicate;
+
+/**
+ * Parse input arguments and creates a new AvailableCommand object.
+ */
+public class AvailableCommandParser implements Parser<AvailableCommand> {
+
+    /**
+     * Parse the given {@code String} of arguments in the context of AvailableCommand
+     * and returns an AvailableCommand for execution.
+     * @param userInput full user input string
+     * @return an AvailableCommand object
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public AvailableCommand parse(String userInput) throws ParseException {
+        ArgumentMultimap argMultimap =
+            ArgumentTokenizer.tokenize(userInput, PREFIX_DATE);
+
+        if (argMultimap.getValue(PREFIX_DATE).isEmpty()) {
+            throw new ParseException(
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AvailableCommand.MESSAGE_USAGE)
+            );
+        }
+
+        Date date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get());
+        DateMatchesPredicate predicate = new DateMatchesPredicate(date);
+
+        return new AvailableCommand(predicate);
+    }
+}
