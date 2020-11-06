2bc1978e58218704692e8e844f35656b5e2fbc18
Low Ming Lim <48002577+Avalionnet@users.noreply.github.com>
2020-11-07 00:58:00 +0800

    Add tests for ChangeCommand and Parser

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -52,10 +52,12 @@ public class CommandTestUtil {
     public static final String VALID_TAG_FRIEND = "friend";
     public static final String VALID_DATE = "20 Nov 2050";
     public static final String DIFF_DATE = "03 August 2050";
+    public static final String ANOTHER_DATE = "12 May 2050";
     public static final String VALID_TIME = "12pm";
     public static final String SAME_TIME = "Afternoon";
     public static final String OVERLAP_TIME = "12:01 pm";
     public static final String VALID_DURATION = "90";
+    public static final long VALID_DURATION_LONG = 90;
     public static final String DIFF_DURATION = "91";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
@@ -75,6 +77,12 @@ public class CommandTestUtil {
     public static final String REMARK_DESC_AMY = PREFIX_REMARK + VALID_REMARK_AMY;
     public static final String REMARK_DESC_BOB = PREFIX_REMARK + VALID_REMARK_BOB;
     public static final String REMARK_DESC_EMPTY = PREFIX_REMARK + " ";
+    public static final String CHANGE_DATE_TIME_DURATION =
+            " " + PREFIX_DATE + VALID_DATE + " " + PREFIX_TIME + VALID_TIME + " "
+            + PREFIX_DURATION + VALID_DURATION;
+    public static final String CHANGE_TIME = " " + PREFIX_TIME + VALID_TIME;
+    public static final String CHANGE_DATE = " " + PREFIX_DATE + VALID_DATE;
+    public static final String CHANGE_DURATION = " " + PREFIX_DURATION + VALID_DURATION;
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_NRIC_DESC = " " + PREFIX_NRIC + "q1234567k"; // lower caps not allowed in Nric
@@ -99,6 +107,7 @@ public class CommandTestUtil {
     public static final EditCommand.EditPatientDescriptor EDIT_DESC_BOB;
     public static final FindCommand.FindPatientDescriptor FIND_DESC_AMY;
     public static final FindCommand.FindPatientDescriptor FIND_DESC_BOB;
+
     public static final DateTimeLoader LOADER;
     public static final DateTimeLoader LOADER_DIFF_TIME;
     public static final DateTimeLoader LOADER_DIFF_DATE;
